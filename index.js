0eec44e8756d251718e8d0b13a50eaf1978a708c
Shuayb-Dev <shuayb.abdulkadir5@gmail.com>
2024-04-09 09:18:13 +0100

    Begun Work On Mobile View

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function ValidateEmail() {
-  event.preventDefault();
-  var input = document.getElementById("input");
-  var validRegex =
+  event.preventDefault(); //Prevents Email Validation Message From Disappearing Straightaway
+
+  let input = document.getElementById("input");
+  let validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   if (input.value.match(validRegex)) {
